f605eeed4bc8bf45d12739f51447687c34199590
first commit <tap135@hotmail.com>
2018-06-19 06:45:32 -0400

    Input Function, and State

--- src/App.js
+++ src/App.js
@@ -19,6 +19,24 @@ const particlesOptions = {
 }
 
 class App extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -29,7 +47,8 @@ class App extends Component {
        <Navigation/>
        <Logo />
        <Rank />
-       <ImageLinkForm />
+       <ImageLinkForm  onInputChange={this.onInputChange} 
+                       onButtonSubmit={this.onButtonSubmit}  />
 
        {/*<Logo />
        <ImageLinkForm />
