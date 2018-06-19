f6c5261f216e38dd8c310201d83230ddc657fb86
first commit <tap135@hotmail.com>
2018-06-18 21:12:01 -0400

    React Node

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Navigation from './components/navigation/Navigation';
+import Logo from './components/logo/Logo';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navigation/>
+       <Logo />
+    
+       {/*<Logo />
+       <ImageLinkForm />
+       <FaceRecognition /> */}
+
+      </div>
+    );
+  }
+}
+
+export default App;
