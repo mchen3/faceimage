da1053ba00c1fd326faa6ac6efaa5f45cbd7fa1d
first commit <tap135@hotmail.com>
2018-06-18 22:12:49 -0400

    Particle.js Background Animation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,29 @@ import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
 import Rank from './components/rank/Rank';
-
-
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Particles className='particles'
+              params={particlesOptions}
+      />
+
        <Navigation/>
        <Logo />
        <Rank />
