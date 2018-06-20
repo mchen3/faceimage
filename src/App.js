80f266bb6e1b0234fedb7de3d3a10e1e092ad8a9
first commit <tap135@hotmail.com>
2018-06-19 21:59:29 -0400

    State Management, Input to Image

--- src/App.js
+++ src/App.js
@@ -30,21 +30,41 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: ''
     }
   }
 
   onInputChange = (event) => {
-    console.log(event.target.value);
+
+    this.setState({ input: event.target.value });
+
+   // console.log(event.target.value);
+   // console.log(this.state);    
+
+   // this.setState({value: event.target.value});
+
   }
 
   onButtonSubmit = () => {
-    //console.log('click');
+    
+    this.setState ({ imageUrl: this.state.input });
+
+    //this.setState ({ imageUrl: this.state.input });
+    //console.log(this.state.input);
+
+   // console.log(this.state);
+
+    //this.setState = ({ imageUrl: 'ccc.com' });
+
+   //this.setState ({ imageUrl: 'http://patogh-plus.com/wp-content/uploads/2017/01/Essential-Landscape-Photography-Tips.jpg' });
+
+    //console.log(this.state);    
 
     app.models.predict(Clarifai.COLOR_MODEL, 
     "https://samples.clarifai.com/face-det.jpg").then(
       function(response) {
         // do something with response
-        console.log(response);
+        //console.log(response);
       },
       function(err) {
         // there was an error
@@ -67,7 +87,7 @@ class App extends Component {
        <ImageLinkForm  onInputChange={this.onInputChange} 
                        onButtonSubmit={this.onButtonSubmit}  />
 
-       <FaceRecognition />
+       <FaceRecognition imageUrl={ this.state.imageUrl }/>
 
       </div>
     );
