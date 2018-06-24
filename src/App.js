cbff6d8a57f09642463406d627e7efd287366051
first commit <tap135@hotmail.com>
2018-06-24 15:28:51 -0400

    Clean up

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const initialState =  {
 class App extends Component {
   constructor() {
     super();
-    
+
     this.state = initialState;
   }
 
@@ -109,6 +109,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count }))
             })
+            .catch(console.log);
           }
          this.displayFaceBox(this.calculateFaceLocation(response))    
        })
